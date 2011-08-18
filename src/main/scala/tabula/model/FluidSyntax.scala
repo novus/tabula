4d93b0a5425628d917bf01815baa15d605bad493
Max Afonov <max@bumnetworks.com>
2011-08-18 18:59:25 -0400

    got the "DSL" to compile

--- src/main/scala/tabula/model/FluidSyntax.scala
+++ src/main/scala/tabula/model/FluidSyntax.scala
@@ -1,137 +1,111 @@
-// package tabula.model
+package tabula.model
 
-// import scala.math.{ BigDecimal => ScalaBigDecimal }
-// import java.math.{ BigDecimal => JavaBigDecimal }
-// import java.text.DecimalFormat
-// import com.novus.commons.util._
-// import com.novus.commons.util.RichBigDecimal._
-// import org.scala_tools.time.Imports._
+import scala.math.{ BigDecimal => ScalaBigDecimal }
+import java.math.{ BigDecimal => JavaBigDecimal }
+import java.text.DecimalFormat
+import org.scala_tools.time.Imports._
 
-// case class CanBeColumn(column: Column, next: Option[CanBeColumn] = None) extends Logging {
-//   def `!:`(x: Any): CanBeColumn = {
-//     any2cbc(x).copy(next = Some(this))
-//   }
+case class CanBeColumn(column: Column, next: Option[CanBeColumn] = None) {
+  def `!:`(x: Any): CanBeColumn = {
+    any2cbc(x).copy(next = Some(this))
+  }
 
-//   def `#:`(x: Any): CanBeColumn = {
-//     x match {
-//       case ((a: Any, url: String), text: Option[String]) => linked_any2cbc(a, url, text).copy(next = Some(this))
-//       case _ => `!:`(x)
-//     }
-//   }
+  def `#:`(x: Any): CanBeColumn = {
+    x match {
+      case ((a: Any, url: String), text: Option[String]) => linked_any2cbc(a, url, text).copy(next = Some(this))
+      case _ => `!:`(x)
+    }
+  }
 
-//   def asColumns: List[Column] = column :: {
-//     next match {
-//       case Some(cbc) => cbc.asColumns
-//       case _         => Nil
-//     }
-//   } ::: Nil
-// }
+  def asColumns: List[Column] = column :: {
+    next match {
+      case Some(cbc) => cbc.asColumns
+      case _         => Nil
+    }
+  } ::: Nil
+}
 
-// object `package` {
-//   val Blank = StringColumn(None)
+object `package` {
+  val Blank = StringColumn(None)
 
-//   implicit def cols2rbd(cols: List[Column]) = new {
-//     private def d0(f: Option[BigDecimal] => Option[BigDecimal]): List[Column] =
-//       cols.map {
-//         case bdc: BigDecimalColumn => bdc.copy(value = f(bdc.value))
-//         case x                     => x
-//       }
+  implicit def bdcpimp(col: Column) = new {
+    def reformat(f: => DecimalFormat): Column = col match {
+      case bdc: BigDecimalColumn => bdc.copy(formatter = f)
+      case _                     => col
+    }
 
-//     def scaleToPct = d0 {
-//       _.scaleToPowerOfTen(2)
-//     }
+    def number: Option[BigDecimal] = col match {
+      case bdc: BigDecimalColumn => bdc.scaled
+      case _                     => None
+    }
 
-//     def scaleToBps = d0 {
-//       _.scaleToPowerOfTen(4)
-//     }
+    def mapNumber(f: Option[BigDecimal] => Option[BigDecimal]): Column = col match {
+      case bdc: BigDecimalColumn => bdc.copy(value = f(bdc.scaled))
+      case _                     => col
+    }
+  }
 
-//     def scaleToThousands = d0 {
-//       _.scaleToPowerOfTen(-3)
-//     }
+  implicit def pimpmanybdcs(cols: List[Column]) = new {
+    def reformat(f: => DecimalFormat): List[Column] = cols.map(_.reformat(f))
 
-//     def scaleToMillions = d0 {
-//       _.scaleToPowerOfTen(-6)
-//     }
-//   }
+    def mapNumber(f: Option[BigDecimal] => Option[BigDecimal]): List[Column] = cols.map(_.mapNumber(f))
+  }
 
-//   implicit def bdcpimp(col: Column) = new {
-//     def reformat(f: => DecimalFormat): Column = col match {
-//       case bdc: BigDecimalColumn => bdc.copy(formatter = f)
-//       case _                     => col
-//     }
+  def linked_any2cbc(a: Any, url: String, text: Option[String]): CanBeColumn = {
+    any2cbc(a) match {
+      case cbc @ CanBeColumn(lc: LinkableColumn, _) => cbc.copy(column = lc.linkTo(url, text, Nil))
+      case cbc                                      => cbc
+    }
+  }
 
-//     def number: Option[BigDecimal] = col match {
-//       case bdc: BigDecimalColumn => bdc.scale
-//       case _                     => None
-//     }
+  implicit def any2cbc(a: Any): CanBeColumn = {
+    if (a.isInstanceOf[CanBeColumn]) a.asInstanceOf[CanBeColumn]
+    else
+      CanBeColumn(column = {
+        a match {
+          case c: Column                  => c
 
-//     def mapNumber(f: Option[BigDecimal] => Option[BigDecimal]): Column = col match {
-//       case bdc: BigDecimalColumn => bdc.copy(value = f(bdc.scale))
-//       case _                     => col
-//     }
-//   }
+          case b: Boolean                 => StringColumn("%s".format(b))
 
-//   implicit def pimpmanybdcs(cols: List[Column]) = new {
-//     def reformat(f: => DecimalFormat): List[Column] = cols.map(_.reformat(f))
+          case s: String                  => StringColumn(Option(s))
+          case Some(os: String)           => StringColumn(os)
 
-//     def mapNumber(f: Option[BigDecimal] => Option[BigDecimal]): List[Column] = cols.map(_.mapNumber(f))
-//   }
+          case bd: ScalaBigDecimal        => BigDecimalColumn(Option(bd))
+          case Some(obd: ScalaBigDecimal) => BigDecimalColumn(Option(obd))
 
-//   def linked_any2cbc(a: Any, url: String, text: Option[String]): CanBeColumn = {
-//     any2cbc(a) match {
-//       case cbc @ CanBeColumn(lc: LinkableColumn, _) => cbc.copy(column = lc.linkTo(url, text, Nil))
-//       case cbc                                      => cbc
-//     }
-//   }
+          case bd: JavaBigDecimal         => BigDecimalColumn(Option(bd).map(ScalaBigDecimal(_)))
+          case Some(jbd: JavaBigDecimal)  => BigDecimalColumn(Option(jbd).map(ScalaBigDecimal(_)))
 
-//   implicit def any2cbc(a: Any): CanBeColumn = {
-//     if (a.isInstanceOf[CanBeColumn]) a.asInstanceOf[CanBeColumn]
-//     else
-//       CanBeColumn(column = {
-//         a match {
-//           case c: Column                  => c
+          case n: Int                     => BigDecimalColumn(Some(ScalaBigDecimal(n)))
+          case l: Long                    => BigDecimalColumn(Some(ScalaBigDecimal(l)))
+          case f: Float                   => BigDecimalColumn(Some(ScalaBigDecimal(f)))
+          case d: Double                  => BigDecimalColumn(Some(ScalaBigDecimal(d)))
 
-//           case b: Boolean                 => StringColumn("%s".format(b))
+          case dt: DateTime               => DateTimeColumn(Option(dt))
+          case Some(odt: DateTime)        => DateTimeColumn(Some(odt))
 
-//           case s: String                  => StringColumn(Option(s))
-//           case Some(os: String)           => StringColumn(os)
+          case e: Enumeration#Value       => StringColumn(e.toString)
 
-//           case bd: ScalaBigDecimal        => BigDecimalColumn(Option(bd))
-//           case Some(obd: ScalaBigDecimal) => BigDecimalColumn(obd)
+          case None                       => EmptyColumn()
+          case null                       => EmptyColumn()
 
-//           case bd: JavaBigDecimal         => BigDecimalColumn(Option(bd))
-//           case Some(obd: JavaBigDecimal)  => BigDecimalColumn(obd)
+          case x if (x != null)           => StringColumn(x.toString)
+        }
+      })
+  }
 
-//           case n: Int                     => BigDecimalColumn(n)
-//           case l: Long                    => BigDecimalColumn(l)
-//           case f: Float                   => BigDecimalColumn(f)
-//           case d: Double                  => BigDecimalColumn(d)
+  implicit def cbc2row(cbc: CanBeColumn): Row = Row(columns = cbc.asColumns)
 
-//           case dt: DateTime               => DateTimeColumn(Option(dt))
-//           case Some(odt: DateTime)        => DateTimeColumn(Some(odt))
+  implicit def cbc2columns(cbc: CanBeColumn): Seq[Column] = cbc.asColumns
 
-//           case e: Enumeration#Value       => StringColumn(e.toString)
+  implicit def things2cbc(as: Seq[_]): Seq[Column] = as.map(any2cbc _).map(_.asColumns.head)
 
-//           case None                       => EmptyColumn()
-//           case null                       => EmptyColumn()
+  implicit def optint2optbd(i: Option[Int]): Option[ScalaBigDecimal] = i.map(ScalaBigDecimal(_))
 
-//           case x if (x != null)           => StringColumn(x.toString) // fallback: just make it a string column
-//         }
-//       })
-//   }
+  def sortIndexable(a: Indexable, b: Indexable) =
+    a.idx.orElse(b.idx).getOrElse(-1) <= b.idx.orElse(a.idx).getOrElse(-1)
+}
 
-//   implicit def cbc2row(cbc: CanBeColumn): Row = Row(columns = cbc.asColumns)
-
-//   implicit def cbc2columns(cbc: CanBeColumn): Seq[Column] = cbc.asColumns
-
-//   implicit def things2cbc(as: Seq[_]): Seq[Column] = as.map(any2cbc _).map(_.asColumns.head)
-
-//   implicit def optint2optbd(i: Option[Int]): Option[ScalaBigDecimal] = i.map(ScalaBigDecimal(_))
-
-//   def sortIndexable(a: Indexable, b: Indexable) =
-//     a.idx.orElse(b.idx).getOrElse(-1) <= b.idx.orElse(a.idx).getOrElse(-1)
-// }
-
-// trait Indexable {
-//   val idx: Option[Int]
-// }
+trait Indexable {
+  val idx: Option[Int]
+}
