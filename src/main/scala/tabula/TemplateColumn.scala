1f0f44a70f2f8a99bc4abb9f0a1f0fab44c9df10
Max Afonov <max@bumnetworks.com>
2011-08-18 19:05:58 -0400

    not needed here

--- src/main/scala/tabula/TemplateColumn.scala
+++
@@ -1,6 +0,0 @@
-package tabula
-
-case class TemplateColumn(template: () => String, attrs: () => Map[String, Any]) extends Column {
-  def format: String =
-    throw new UnsupportedOperationException("format on TemplateColumn doesn't make sense")
-}
