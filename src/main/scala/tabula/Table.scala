5f2c6a58769e528951fe7f19c4f00f632c0be696
Max Afonov <max@bumnetworks.com>
2011-12-06 22:43:07 -0500

    compressed header/footer garbage back into TableModel proper

--- src/main/scala/tabula/Table.scala
+++ src/main/scala/tabula/Table.scala
@@ -1,5 +1,3 @@
 package tabula
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
-
 case class Table(name: String, header: Option[Row] = None, rows: List[Row], footer: Option[Row] = None)
--- src/main/scala/tabula/TableModel.scala
+++ src/main/scala/tabula/TableModel.scala
@@ -4,41 +4,23 @@ trait TableModel[F] {
   self =>
 
   def columns: List[Column[F]]
-  def header: Option[Row]
-  def rows(xs: List[F]): List[Row] =
-    xs.map(Option(_)).map(x => Row(columns.flatMap(_.cellOrBlank(x))))
-  def footer(xs: List[F], rows: List[Row]): Option[Row]
-  def table(xs: List[F]) = {
-    val table = Table(name = "", header = header, rows = rows(xs))
-    table.copy(footer = footer(xs, table.rows))
-  }
-}
-
-object TableModel {
-  def apply[F](cs: List[Column[F]]) =
-    new TableModel[F] with TableModelWithHeader[F] with TableModelWithFooter[F] { val columns = cs }
-}
-
-trait TableModelWithHeader[F] {
-  self: TableModel[F] =>
 
   private lazy val header_? = columns.exists { case Named(_, _, _) => true case _ => false }
 
-  override lazy val header =
+  lazy val header =
     if (header_?)
       Some(Row(columns.map {
         case Named(_, name, label) => StringCell(label.getOrElse(name))
         case _                     => Blank
       }))
     else None
-}
 
-trait TableModelWithFooter[F] {
-  self: TableModel[F] =>
+  def rows(xs: List[F]): List[Row] =
+    xs.map(Option(_)).map(x => Row(columns.flatMap(_.cellOrBlank(x))))
 
   private lazy val footer_? = columns.exists { case _: Aggregated[_, _] => true case _ => false }
 
-  override def footer(xs: List[F], rows: List[Row]) =
+  def footer(xs: List[F], rows: List[Row]) =
     if (footer_?)
       Some(Row(columns.zipWithIndex.map {
         case (agg: Aggregated[F, Cell], idx) => {
@@ -47,4 +29,13 @@ trait TableModelWithFooter[F] {
         case _ => Blank
       }))
     else None
+
+  def table(xs: List[F]) = {
+    val table = Table(name = "", header = header, rows = rows(xs))
+    table.copy(footer = footer(xs, table.rows))
+  }
+}
+
+object TableModel {
+  def apply[F](cs: List[Column[F]]) = new TableModel[F] { val columns = cs }
 }
