1df3be021b8f8ec8a5b8d13c340d66d44fc028df
Max Afonov <max@bumnetworks.com>
2012-02-28 01:05:35 -0500

    xsbt 0.11.x

--- project/TabulaBuild.scala
+++ project/TabulaBuild.scala
@@ -1,98 +1,90 @@
 import sbt._
 import Keys._
-import ScalariformPlugin.{format, formatPreferences}
+import com.typesafe.sbtscalariform.ScalariformPlugin
+import com.typesafe.sbtscalariform.ScalariformPlugin.ScalariformKeys
+import com.github.siasia.WebPlugin._
+import com.github.siasia.PluginKeys._
+import ScalatePlugin._
 
 object Versions {
-  val ScalaVersion = "2.8.1"
+  val ScalaVersion = "2.9.1"
+  val ScalaTimeVersion = "0.5"
+  val SpecsVersion = "1.6.9"
+  val ScalazVersion = "6.0.4"
   val PoiVersion = "3.7"
-  val SpecsVersion = "1.6.7"
-  val ScalaTimeVersion = "0.4"
 }
 
 object BuildSettings {
   import Versions._
+
+  def prompt(state: State) =
+    "[%s]> ".format(Project.extract(state).currentProject.id)
 
   lazy val buildSettings = Defaults.defaultSettings ++ formatSettings ++ Seq(
     organization := "tabula",
     version := "0.0.1-SNAPSHOT",
     scalaVersion := ScalaVersion,
     scalacOptions ++= Seq("-deprecation",  "-unchecked"),
-    publishTo <<= version {
-      case v: String =>
-        Some(Resolver.file("repo", file("../repo/%s".format({ if (v.endsWith("-SNAPSHOT")) "snapshots" else "releases" }))))
-    },
+    shellPrompt := prompt,
     showTiming := true,
     parallelExecution := true,
-    credentials += Credentials(Path.userHome / ".ivy2" / ".novus_nexus"),
-    ivyXML := ivyXml,
     parallelExecution in Test := false,
     testFrameworks += TestFrameworks.Specs,
     libraryDependencies += Deps.specs,
-    resolvers ++= Seq(Resolvers.novusRels, Resolvers.novusSnaps, Resolvers.akkaRepo),
-    offline := true
+    resolvers ++= Resolvers.All,
+    offline := false
   )
 
-  lazy val formatSettings = ScalariformPlugin.settings ++ Seq(
-    formatPreferences in Compile := formattingPreferences,
-    formatPreferences in Test    := formattingPreferences
+  lazy val formatSettings = ScalariformPlugin.scalariformSettings ++ Seq(
+    ScalariformKeys.preferences in Compile := formattingPreferences,
+    ScalariformKeys.preferences in Test    := formattingPreferences
   )
 
   lazy val formattingPreferences = {
     import scalariform.formatter.preferences._
-    FormattingPreferences().setPreference(AlignParameters, true).
-      setPreference(AlignSingleLineCaseStatements, true).
-      setPreference(CompactControlReadability, true). // waiting for CCR patch to go mainstream, patiently patiently
-      setPreference(CompactStringConcatenation, true).
-      setPreference(DoubleIndentClassDeclaration, true).
-      setPreference(FormatXml, true).
-      setPreference(IndentLocalDefs, true).
-      setPreference(IndentPackageBlocks, true).
-      setPreference(IndentSpaces, 2).
-      setPreference(MultilineScaladocCommentsStartOnFirstLine, true).
-      setPreference(PreserveSpaceBeforeArguments, false).
-      setPreference(PreserveDanglingCloseParenthesis, false).
-      setPreference(RewriteArrowSymbols, false).
-      setPreference(SpaceBeforeColon, false).
-      setPreference(SpaceInsideBrackets, false).
-      setPreference(SpacesWithinPatternBinders, true)
-  }
-
-  lazy val ivyXml = {
-    <dependencies>
-    <exclude org="se.scalablesolutions.akka" module="akka-persistence"/>
-    <exclude org="se.scalablesolutions.akka" module="akka-spring"/>
-    <exclude org="se.scalablesolutions.akka" module="akka-amqp"/>
-    <exclude org="org.guiceyfruit" module="*"/>
-    </dependencies>
+    FormattingPreferences().
+    setPreference(AlignParameters, true).
+    setPreference(AlignSingleLineCaseStatements, true).
+    setPreference(CompactControlReadability, true).
+    setPreference(CompactStringConcatenation, true).
+    setPreference(DoubleIndentClassDeclaration, true).
+    setPreference(FormatXml, true).
+    setPreference(IndentLocalDefs, true).
+    setPreference(IndentPackageBlocks, true).
+    setPreference(IndentSpaces, 2).
+    setPreference(MultilineScaladocCommentsStartOnFirstLine, true).
+    setPreference(PreserveSpaceBeforeArguments, false).
+    setPreference(PreserveDanglingCloseParenthesis, false).
+    setPreference(RewriteArrowSymbols, false).
+    setPreference(SpaceBeforeColon, false).
+    setPreference(SpaceInsideBrackets, false).
+    setPreference(SpacesWithinPatternBinders, true)
   }
 }
 
 object Resolvers {
-  val akkaRepo = "Akka Repo" at "http://akka.io/repository"
-  val novusRels = "Novus Releases" at "http://repo.novus.com/releases"
-  val novusSnaps = "Novus Snapshots" at "http://repo.novus.com/snapshots"
+  val All = Seq(
+    "Novus Releases" at "http://repo.novus.com/releases",
+    "Novus Snapshots" at "http://repo.novus.com/snapshots",
+    "Coda Hale's repo" at "http://repo.codahale.com",
+    "localRels" at "file:/home/max/work/repo/releases",
+    "localSnaps" at "file:/home/max/work/repo/snapshots")
 }
 
 object Deps {
   import Versions._
 
   val time = "org.scala-tools.time" %% "time" % ScalaTimeVersion
-  val poi = "org.apache.poi" % "poi" % PoiVersion
   val specs = "org.scala-tools.testing" %% "specs" % SpecsVersion % "test"
-}
+  val scalaz = "org.scalaz" %% "scalaz-core" % ScalazVersion
+  val poi = "org.apache.poi" % "poi" % PoiVersion
 
-object ProjectDeps {
-  import Deps._
-
-  val deps = Seq(time, poi, specs)
+  val TabulaDeps = Seq(time, specs, scalaz, poi)
 }
 
 object TabulaBuild extends Build {
   import BuildSettings._
-  import ProjectDeps._
+  import Deps._
 
-  lazy val util = Project(
-    "tabula", file("."),
-    settings = buildSettings ++ Seq(libraryDependencies ++= deps)
-  )
+  lazy val tabula = Project(id = "tabula", base = file("."), settings = buildSettings ++ Seq(libraryDependencies ++= TabulaDeps))
 }
--- project/plugins/build.sbt
+++
@@ -1,3 +0,0 @@
-resolvers += "Novus Snapshots Repository" at "http://repo.novus.com/snapshots/"
-
-libraryDependencies += "com.typesafe.sbt-scalariform" %% "sbt-scalariform" % "0.1.3.1-SNAPSHOT"
