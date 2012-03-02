4f891af0b9165fe8f0a17ddad8d1e74245c35b8d
Max Afonov <max@bumnetworks.com>
2012-03-02 08:31:28 -0500

    errr...

--- json/src/test/scala/tabula/json/test/JsonSpec.scala
+++ json/src/test/scala/tabula/json/test/JsonSpec.scala
@@ -6,13 +6,14 @@ import tabula.json._
 import tabula.test._
 import org.specs._
 import net.liftweb.json._
+import net.liftweb.json.{ pretty => prettyJson }
 import net.liftweb.json.JsonDSL._
 
 class JsonSpec extends Specification {
   "a JSON output" should {
     "produce JSON" in {
       import ShowcaseSpec._
-      render(JSON(table))
+      println(prettyJson(render(JSON(table))))
     }
   }
 }
