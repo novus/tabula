652a0f587f143b3791199c1fa89f74b24404269a
Max Afonov <max@bumnetworks.com>
2013-10-07 08:06:55 -0400

    v0.0.4 release

--- project/TabulaBuild.scala
+++ project/TabulaBuild.scala
@@ -23,7 +23,7 @@ object BuildSettings {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.bumnetworks",
-    version := "0.0.4-SNAPSHOT",
+    version := "0.0.4",
     scalaVersion := ScalaVersion,
     scalacOptions ++= Seq("-deprecation",  "-unchecked", "-feature", "-language:implicitConversions", "-language:reflectiveCalls"),
     shellPrompt := prompt,
