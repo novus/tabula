4f64a56716ded665f4260ef80c40cd9ba565a942
Max Afonov <max@bumnetworks.com>
2013-10-15 15:12:09 -0400

    bump

--- project/TabulaBuild.scala
+++ project/TabulaBuild.scala
@@ -23,7 +23,7 @@ object BuildSettings {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.bumnetworks",
-    version := "0.0.5",
+    version := "0.0.6-SNAPSHOT",
     scalaVersion := ScalaVersion,
     scalacOptions ++= Seq("-deprecation",  "-unchecked", "-feature", "-language:implicitConversions", "-language:reflectiveCalls"),
     shellPrompt := prompt,
